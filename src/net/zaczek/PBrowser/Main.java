8f096b9a3c00f6d464307e455a2e995861ce6627
arthur <arthur.zaczek@gmail.com>
2011-08-03 20:02:04 +0200

    About, new Version

--- src/net/zaczek/PBrowser/Main.java
+++ src/net/zaczek/PBrowser/Main.java
@@ -10,12 +10,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class Main extends ListActivity {
+	
+	private static final int ABOUT_ID = 1;
+	private static final int EXIT_ID = 2;
+	
 	private ArrayAdapter<String> adapter;
 	/** Called when the activity is first created. */
 	@Override
@@ -62,5 +68,28 @@ public class Main extends ListActivity {
 		} catch (Exception ex) {
 			Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
 		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, ABOUT_ID, 0, "About");
+		menu.add(0, EXIT_ID, 0, "Exit");
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		int itemId = item.getItemId();
+		switch (itemId) {
+		case ABOUT_ID:
+			startActivity(new Intent(this, About.class));
+			return true;
+		case EXIT_ID:
+			finish();
+			return true;
+		}
+
+		return super.onMenuItemSelected(featureId, item);
 	}
 }
